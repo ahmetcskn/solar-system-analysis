9b9dfe144e4b3fb4d2729294e329b1d9b943b8a7
ahmetcskn <59967778+ahmetcskn@users.noreply.github.com>
2025-01-26 00:42:01 +0300

    Update distance_period_regression.py

--- distance_period_regression.py
+++ distance_period_regression.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('solar_system_orbits.csv')
 
-def gradiant_descent(m_now, b_now, points, L):
-    m_gradient = 0
+def gradiant_descent(w_now, b_now, points, L):
+    w_gradient = 0
     b_gradient = 0
 
     n = len(points)
@@ -13,27 +13,27 @@ def gradiant_descent(m_now, b_now, points, L):
         x = points.iloc[i].Distance_from_Sun_AU
         y = points.iloc[i].Orbital_Period_Days
 
-        m_gradient += -(2/n) * x * (y-(m_now * x + b_now))
-        b_gradient += -(2 / n) * (y - (m_now * x + b_now))
+        w_gradient += -(2/n) * x * (y-(w_now * x + b_now))
+        b_gradient += -(2 / n) * (y - (w_now * x + b_now))
 
-    m = m_now - m_gradient * L
+    w = w_now - w_gradient * L
     b = b_now - b_gradient * L
-    return m,b
+    return w,b
 
-m = 0
+w = 0
 b = 0
 L = 0.0001
 epochs = 500
 
 for i in range(epochs):
-    m, b = gradiant_descent(m, b, data, L)
+    w, b = gradiant_descent(w, b, data, L)
 
-print(m, b)
+print(w, b)
 
 x_min = data.Distance_from_Sun_AU.min()
 x_max = data.Distance_from_Sun_AU.max()
 x_range = [x for x in range(int(x_min), int(x_max) + 1)]
 
 plt.scatter(data.Distance_from_Sun_AU, data.Orbital_Period_Days, color="black")
-plt.plot(list(x_range), [m * x + b for x in x_range], color="red")
+plt.plot(list(x_range), [w * x + b for x in x_range], color="red")
 plt.show()
